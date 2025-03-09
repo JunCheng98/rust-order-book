cf8d6446398b83210990afef57116ec184591b9b
codefarmer <junchengillusian@gmail.com>
2025-03-10 02:04:27 +0800

    refactor modules and driver code

--- src/main.rs
+++ src/main.rs
@@ -3,47 +3,39 @@ use env_logger::Builder;
 use futures_util::StreamExt;
 use log;
 
-use std::env;
-
-mod model;
-mod endpoints;
 mod websocket;
-mod stream;
-mod manager;
-mod formatter;
+mod response;
+mod orderbook;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let args: Vec<String> = env::args().collect();
-    let query = &args[1];
+    let args: Vec<String> = std::env::args().collect();
+    let symbol = match args.get(1) {
+        Some(arg) => arg,
+        None => panic!("Run the program as follows: cargo run -- [SYMBOL]\n Symbol examples: BTCUSDT, BNBBTC etc.")
+    };
 
     Builder::from_default_env()
         .filter(None, log::LevelFilter::Info)
         .init();
 
-    // fetch the current order book from provided symbol
-    let mut order_book = manager::init_order_book(query).await;
+    let mut order_book = orderbook::OrderBook::new(symbol.to_string());
 
-    // Websocket
-    let mut conn = websocket::init(query).await;
-
-    // Read messages
+    let mut conn = websocket::init_connection(symbol.as_str()).await;
     while let Some(message) = conn.as_mut().next().await {
         match message {
             Ok(message) => {
-                stream::handle_stream_data(message, &mut order_book).await;
+                websocket::handle_stream_data(message, &mut order_book).await;
+                println!("Top Level: {:?}", order_book.get_best_bid_ask());
+                println!("{}", order_book.to_string());
             }
             Err(err) => {
                 log::error!("Error when reading stream: {}", err);
                 break
             }
         }
-
-        formatter::print_order_book(&order_book, &query);
     }
-
-    // Disconnect
     conn.close().await.expect("Failed to disconnect");
 
     Ok(())
